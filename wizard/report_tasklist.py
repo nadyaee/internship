b1f0b3abbaf7a461d4f0070eb5df815c025bf866
nadya <nadya@290f1bea-3598-11ed-b389-0d202bf434bf>
2022-09-23 02:37:47 +0000

    folder wizard

---
+++ wizard/report_tasklist.py
@@ -0,0 +1,23 @@
+from odoo import models, fields, api
+import time
+
+class ReportAllTasklist(models.TransientModel):
+    _name = "report.all.tasklist"
+
+    intern_id = fields.Many2one('intern.directory', 'Name')
+    date_from = fields.Date('Date from', store=True, required=True)
+    date_to = fields.Date('Date to', store=True, required=True)
+
+    def action_print_tasklist_report(self):
+        data = {
+            'form': self.read()[0],
+            'model' : 'report.all.tasklist',
+            'date_from' : self.date_from,
+            'date_to' : self.date_to,
+            'intern_id' : self.intern_id.id,
+
+        }
+        report_action = self.env.ref('v11_internship.action_report_tasklist').report_action(self, data=data)
+        print('report', report_action)
+        return report_action
+    
